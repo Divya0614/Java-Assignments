49c4821fb7964b4d3cd50ab5f69902f782e5f385
Divya M <divyam0601@gmail.com>
2026-01-02 12:02:57 +0530

    Create Increment.java

---
+++ Assignment3/Increment.java
@@ -0,0 +1,14 @@
+package com.index;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		int num = 1;
+		while(num<=10) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
