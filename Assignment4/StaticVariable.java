e243c71c7901a23f89d22cf10a5aff802afcf628
Divya M <divyam0601@gmail.com>
2026-01-02 22:27:44 +0530

    Create StaticVariable.java

---
+++ Assignment4/StaticVariable.java
@@ -0,0 +1,17 @@
+package com.index;
+
+public class StaticVariable {
+
+	static int a = 10;
+	static final int b = 20;
+	
+	public static void main(String[] args) {
+		
+	
+		System.out.println("Result" + StaticVariable.a);
+		
+		System.out.println("Result" + StaticVariable.b);
+
+	}
+
+}
