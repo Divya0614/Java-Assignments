4a3de9618204d42b40404376b7e8eef2dcba0436
Divya M <divyam0601@gmail.com>
2026-01-02 12:03:50 +0530

    Create salary.java

---
+++ Assignment3/salary.java
@@ -0,0 +1,22 @@
+package com.index;
+
+public class AssignementOperator {
+
+	public static void main(String[] args) {
+		double salary = 20000;
+		
+		salary += 1000;
+		System.out.println("Total salary" + salary);
+		
+		salary -= 500;
+		System.out.println("Total salary" + salary);
+		
+		salary *= 3;
+		System.out.println("Total salary" + salary);
+		
+		salary /=5;
+		System.out.println("Total salary" + salary);
+
+	}
+
+}
